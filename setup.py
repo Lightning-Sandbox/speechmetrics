157e738ccfd260d7185f52f1fcd3cdab100f98e3
Jirka Borovec <6035284+Borda@users.noreply.github.com>
2023-04-24 13:41:55 +0200

    update requirements

--- setup.py
+++ setup.py
@@ -15,13 +15,11 @@ setup(
         'resampy',
         'pystoi',
         'museval',
-        # This is requred, but srmrpy pull it in,
-	    # and there is a pip3 conflict if we have the following
-	    # line.
+        # This is requred, but srmrpy pull it in, and there is a pip3 conflict if we have the following line.
         #'gammatone @ git+https://github.com/detly/gammatone',
-        'pypesq @ git+https://github.com/vBaiCai/python-pesq',
-        'srmrpy @ git+https://github.com/jfsantos/SRMRpy',
-        'pesq @ git+https://github.com/ludlows/python-pesq',
+        'pypesq @ https://github.com/Lightning-Sandbox/python-pesq/archive/refs/heads/master.zip',
+        'srmrpy @ https://github.com/jfsantos/SRMRpy/archive/refs/heads/master.zip',
+        'pesq @ https://github.com/ludlows/PESQ/archive/refs/heads/master.zip',
     ],
     extras_require={
         'cpu': ['tensorflow>=2.0.0', 'librosa'],
